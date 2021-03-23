4fcd7045bf5d98df7b38744c56389259bfb23e09
Denis <kabachuk_denis@mail.ru>
2021-03-23 19:19:23 +0300

    Updates

---
+++ precache-manifest.a7bc067da638368cd613a7a096213c64.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "662cf9ee3b932530c594506c3a18e158",
+    "url": "/fox-messager/index.html"
+  },
+  {
+    "revision": "4198581733dba3455845",
+    "url": "/fox-messager/static/css/3.49fed19b.chunk.css"
+  },
+  {
+    "revision": "5fffb2c6883ec76f0735",
+    "url": "/fox-messager/static/css/4.af5a4d3e.chunk.css"
+  },
+  {
+    "revision": "e16470f082446a7093b9",
+    "url": "/fox-messager/static/css/5.9062a1dd.chunk.css"
+  },
+  {
+    "revision": "8ca6562845d7041d8faa",
+    "url": "/fox-messager/static/css/main.987e880a.chunk.css"
+  },
+  {
+    "revision": "3646d8975795ec6ab0b2",
+    "url": "/fox-messager/static/js/2.7f1a0410.chunk.js"
+  },
+  {
+    "revision": "b42f36b5d572c611b888796d07d4a679",
+    "url": "/fox-messager/static/js/2.7f1a0410.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4198581733dba3455845",
+    "url": "/fox-messager/static/js/3.66074f2f.chunk.js"
+  },
+  {
+    "revision": "5fffb2c6883ec76f0735",
+    "url": "/fox-messager/static/js/4.cff17ea6.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/fox-messager/static/js/4.cff17ea6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e16470f082446a7093b9",
+    "url": "/fox-messager/static/js/5.2d0fd78a.chunk.js"
+  },
+  {
+    "revision": "8ca6562845d7041d8faa",
+    "url": "/fox-messager/static/js/main.0fc287fe.chunk.js"
+  },
+  {
+    "revision": "aa65c2b14db734cc7a62",
+    "url": "/fox-messager/static/js/runtime-main.191cbad3.js"
+  },
+  {
+    "revision": "a6d538dba42bbcdfc78b6673e618987d",
+    "url": "/fox-messager/static/media/logo.a6d538db.svg"
+  },
+  {
+    "revision": "ea5597632a78b44d7669e496ee5d59bc",
+    "url": "/fox-messager/static/media/preloader.ea559763.svg"
+  },
+  {
+    "revision": "c819cf0fa630f32059cf4175b411deab",
+    "url": "/fox-messager/static/media/user.c819cf0f.jpg"
+  }
+]);
